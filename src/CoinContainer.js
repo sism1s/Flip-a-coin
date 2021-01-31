3d484edeaedc4ae9fb1031b5dc116f496b1ea1b1
sism1s <marko.lazic880@gmail.com>
2021-01-31 23:18:05 +0000

    update

---
+++ src/CoinContainer.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { choice } from "./helpers";
+import Coin from "./Coin";
+
+class CoinContainer extends Component {
+  static defaultProps = {
+    coins: [
+      {
+        side: "heads",
+        imgSrc:
+          "https://www.leftovercurrency.com/app/uploads/2016/11/5-croatian-kuna-coin-obverse-1.jpg",
+      },
+      {
+        side: "tails",
+        imgSrc:
+          "https://www.leftovercurrency.com/app/uploads/2016/11/5-croatian-kuna-coin-reverse-1-100x100.jpg",
+      },
+    ],
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currCoin: null,
+      nFlips: 0,
+      nHeads: 0,
+      nTails: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  flipCoin() {
+    const newCoin = choice(this.props.coins);
+    this.setState((st) => {
+      return {
+        currCoin: newCoin,
+        nFlips: st.nFlips + 1,
+        nHeads: st.nHeads + (newCoin.side === "heads" ? 1 : 0),
+        nTails: st.nTails + (newCoin.side === "tails" ? 1 : 0),
+      };
+    });
+  }
+
+  handleClick(e) {
+    this.flipCoin();
+  }
+
+  render() {
+    return (
+      <div className="CoinContainer">
+        <h2>Let's Flip A Coin!</h2>
+        {this.state.currCoin && <Coin info={this.state.currCoin} />}
+        <button onClick={this.handleClick}>Flip Me!</button>
+        <p>
+          Out of {this.state.nFlips} flips, there have been {this.state.nHeads}{" "}
+          heads and {this.state.nTails} tails.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default CoinContainer;
